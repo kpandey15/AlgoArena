34794e12afab75514576fb1ac5afb6d7f3b80f26
Kohina <pandeykohina@gmail.com>
2024-12-03 20:04:24 +0530

    setup

---
+++ AlgoArena/src/main/java/com/example/AlgoArena/AlgoArenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlgoArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoArenaApplication.class, args);
+	}
+
+}
---
+++ AlgoArena/src/test/java/com/example/AlgoArena/AlgoArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlgoArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
